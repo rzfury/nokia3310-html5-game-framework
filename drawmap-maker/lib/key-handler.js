5c78857fd5382f510bac11b8cb9ce1106ba55646
Ilyas Ilmana <48477044+rzfury@users.noreply.github.com>
2020-01-02 13:54:51 +0700

    add drawmap maker

---
+++ drawmap-maker/lib/key-handler.js
@@ -0,0 +1,120 @@
+let keyState = {
+    UP: {
+        registeredKey: 'ArrowUp',
+        pressed: false
+    },
+    DOWN: {
+        registeredKey: 'ArrowDown',
+        pressed: false
+    },
+    LEFT: {
+        registeredKey: 'ArrowLeft',
+        pressed: false
+    },
+    RIGHT: {
+        registeredKey: 'ArrowRight',
+        pressed: false
+    },
+    ESCAPE: {
+        registeredKey: 'Escape',
+        pressed: false
+    }
+}
+
+/**
+ * Handler when a key is pressed
+ * 
+ * @param {KeyboardEvent} event
+ */
+function KeyDownHandle(event) {
+    for (let key in keyState) {
+        if (keyState[key].registeredKey == event.key) {
+            keyState[key].pressed = true;
+        }
+    }
+}
+
+/**
+ * Handler when a key is released
+ * 
+ * @param {KeyboardEvent} event
+ */
+function KeyUpHandle(event) {
+    for (let key in keyState) {
+        if (keyState[key].registeredKey == event.key) {
+            keyState[key].pressed = false;
+        }
+    }
+}
+
+/**
+ * Register a key to the `KeyState`
+ * 
+ * @param {string} keyStateName Key name for new `KeyState`
+ * @param {string} keyCode Keyboard Event KeyCode
+ */
+function RegisterKey(keyStateName, keyCode) {
+    const keyName = keyStateName.toUpperCase();
+    if (keyState.hasOwnProperty(keyName)) {
+        console.error(`"${keyStateName}" is already exists in keyState`);
+        return false;
+    } else {
+        Object.assign(keyState, {
+            [keyName]: {
+                registeredKey: keyCode,
+                pressed: false
+            }
+        });
+        return keyState[keyName];
+    }
+}
+
+/**
+ * Remove a key from `keyState`
+ * 
+ * @param {string} keyStateName Key name for new `KeyState`
+ */
+function UnregisterKey(keyStateName) {
+    const keyName = keyStateName.toUpperCase();
+    if (keyState.hasOwnProperty(keyName)) {
+        delete keyState[keyName];
+    }
+    else {
+        console.error(`"${keyStateName}" is undefined on keyState.`);
+    }
+}
+
+/**
+ * Get key pressed state
+ * 
+ * @param {string} keyStateName KeyState Name
+ */
+function IsPressed(keyStateName) {
+    const keyName = keyStateName.toUpperCase();
+    if (keyState.hasOwnProperty(keyName)) {
+        return keyState[keyName].pressed;
+    }
+    else {
+        console.error(`"${keyStateName}" is undefined in keyState.`);
+        return false;
+    }
+}
+
+function _InitKeyHandler(additionalDownHandle, additionalUpHandle) {
+    window.addEventListener('keydown', (e) => {
+        if (document.activeElement !== optFilename) {
+            if (!e.ctrlKey) e.preventDefault();
+
+            KeyDownHandle(e);
+            additionalDownHandle && additionalDownHandle(e);
+        }
+    });
+    window.addEventListener('keyup', (e) => {
+        if (document.activeElement !== optFilename) {
+            if (!e.ctrlKey) e.preventDefault();
+
+            KeyUpHandle(e);
+            additionalUpHandle && additionalUpHandle(e);
+        }
+    });
+}
---
+++ drawmap-maker/main.js
@@ -0,0 +1,104 @@
+let canvas, context;
+let optColor, optFilename;
+
+let crosshairPos = { x: 0, y: 0 };
+
+let borderRects = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+};
+
+let drawCoords = [];
+let coordsColor = [];
+
+let color = {
+    dark: '#43523d',
+    light: '#c7f0d8',
+}
+
+let option = {
+    color: color.dark,
+    autoFillColor: 1,
+}
+
+function GenerateDrawMap() {
+    const drawWidth = borderRects.right - borderRects.left + 1;
+    const drawHeight = borderRects.bottom - borderRects.top + 1;
+
+    let drawArrays = new Array(drawHeight);
+    for (let h = 0; h < drawArrays.length; h++) {
+        const drawRow = new Array(drawWidth);
+        for (let i = 0; i < drawRow.length; i++) {
+            const stringPos = JSON.stringify({
+                x: borderRects.left + i,
+                y: borderRects.top + h,
+            });
+            const pixelIndex = drawCoords.indexOf(stringPos);
+
+            drawRow[i] = pixelIndex >= 0 ? coordsColor[pixelIndex] : option.autoFillColor;
+        }
+        drawArrays[h] = drawRow;
+    }
+
+    console.log(drawArrays);
+
+    const texts = JSON.stringify(drawArrays).replace(/\"/g, '');
+
+    const textFileAsBlob = new Blob([`const ${optFilename.value} = ${texts};`], { type: 'text/plain' });
+    const downloadLink = document.createElement("a");
+    downloadLink.download = optFilename.value + '.js';
+    if (window.webkitURL != null) {
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    } else {
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = () => downloadLink.parentNode.removeChild(downloadLink);
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    }
+
+    downloadLink.click();
+}
+
+function SwitchDrawingColor() {
+    option.color = (option.color === color.dark ? color.light : color.dark);
+}
+
+function ManualKeyDownHandler(e) {
+    if (e.key.toLowerCase() === 'x') {
+        SwitchDrawingColor();
+    }
+}
+
+function _Init() {
+    canvas = document.getElementById('drawmap-canvas');
+    context = canvas.getContext('2d');
+    optColor = document.getElementById('opt-color-draw');
+    optFilename = document.getElementById('opt-filename');
+
+    canvas.addEventListener('mousemove', ControlMouseCrossHair, false);
+    canvas.addEventListener('click', AddPixel, false);
+    canvas.oncontextmenu = RemovePixel;
+
+    _InitKeyHandler(ManualKeyDownHandler);
+
+    _Renderer();
+    _UpdateOption();
+}
+
+function _Renderer() {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+    DrawDrawingCoords();
+    DrawBorderRectangle();
+    DrawMouseCrossHair();
+
+    requestAnimationFrame(_Renderer);
+}
+
+function _UpdateOption() {
+    optColor.style.backgroundColor = option.color;
+
+    setTimeout(_UpdateOption, 1);
+}
